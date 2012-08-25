742fc195ed0362e1b965f55c41a777781b9f4a63
Skaradams <skaradams69@gmail.com>
2012-08-25 18:28:53 +0200

    added platformcages

--- rat.py
+++ rat.py
@@ -9,12 +9,20 @@ class Rat(Actor):
         super(Rat, self).__init__(
             'sprite.rat', 'stance', position, size
         )
-        # self.listen_key('right')
+        self._walk_vel = 3.0
+        self._run_multiple = 1.5
+        self._x_vel = 0
+
+        self.listen_key('right')
         # self.listen_key('left')
         # self.listen_key('space')
 
     def update(self):
         super(Rat, self).update()
-  
+        self.set_x_velocity(self._x_vel)
+
+    def on_right_pressed(self):
+        self._x_vel += self._walk_vel
+    
     def on_collision(self, chunk, point):
         pass
