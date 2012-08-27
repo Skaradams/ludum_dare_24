d85fd0d82a1bd3b0f807a14071c9429c633f6bd4
Skaradams <skaradams69@gmail.com>
2012-08-27 12:57:07 +0200

    added comicstrips

--- levels/lab.py
+++ levels/lab.py
@@ -13,6 +13,7 @@ from platforms.start import Start
 from platforms.flag import Flag
 
 from menus.ingamemenu import InGameMenu
+from comicstrip import ComicStrip
 
 from pill import *
 from rat import Rat
@@ -114,9 +115,9 @@ class Lab(Level):
     def on_frame(self, delta):
         super(Lab, self).on_frame(delta)
         if self._rat.contains(self._end) and self._next_level != None:
-            self._navigator.set_current_view(self._next_level(self._resolution, self._navigator))
+            # self._navigator.set_current_view(self._next_level(self._resolution, self._navigator))
+            self._navigator.set_current_view(ComicStrip(self._next_level(self._resolution, self._navigator)))
         self.pill_spawn()
-
         
     def change_rat(self, new_rat):
         self.remove_chunk(self._rat)
